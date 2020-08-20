ff6e34bf4bc6eea13428a10f30e459b65a07ee66
Alex Nguyen <anguyen@thunderhead.com>
2020-08-20 17:22:12 -0500

    Add One SDK methods

--- ionic4/src/app/app.component.ts
+++ ionic4/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare var window:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -19,6 +21,25 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+
+      var one = window.One;
+      
+      if (one) {
+          one.init({
+                  siteKey: "ONE-XXXXXXXXXX-1022",
+                  touchpointURI: "myAppsNameURI",
+                  apiKey: "f713d44a-8af0-4e79-ba7e-xxxxxxxxx",
+                  sharedSecret: "bb8bacb2-ffc2-4c52-aaf4-xxx",
+                  userId: "yourUsername@yourCompanyName",
+                  adminMode: false,
+                  hostName: "https://xx.thunderhead.com"
+                  }
+                  );
+          one.sendInteraction('/App', null);
+          one.sendBaseTouchpointProperties({key:'value'});
+          one.sendInteractionForOutboundLink('https://www.thunderhead.com/?key=value');
+      }
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
