da2788567525e737a5872d8d76dd6d545cbc50b5
Alex Nguyen <anguyen@thunderhead.com>
2020-08-31 10:36:55 -0600

    Delete old Cordova sample projects

--- hello-cordova/www/js/config/one.js
+++
@@ -1,9 +0,0 @@
-define({
-      siteKey: "<YOUR-SITE-KEY>",
-      touchpointURI: "<YOUR-TOUCHPOINT-URI>",
-      apiKey: "<YOUR-API-KEY>",
-      sharedSecret: "<YOUR-SHARED-SECRET>",
-      userId: "<YOUR-USER-ID>",
-      adminMode: false,
-      hostName: "<YOUR-HOSTNAME>"
-});
--- ionic2/Hacker News/app/config/one.js
+++
@@ -1,9 +0,0 @@
-export const ONE_PARAMETERS = {
-    "siteKey": "<YOUR-SITE-KEY>",
-    "touchpointURI" : "<YOUR-TOUCHPOINT-URI>",
-    "apiKey" : "<YOUR-API-KEY>",
-    "sharedSecret" : "<YOUR-SHARED-SECRET>",
-    "userId" : "<YOUR-USER-ID>",
-    "adminMode" : false,
-    "hostName" : "<YOUR-HOSTNAME>",
-}
