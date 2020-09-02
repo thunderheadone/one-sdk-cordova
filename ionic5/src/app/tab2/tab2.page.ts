b9e094bb8b7b507fa54ca3866b426b1522367537
Alex Nguyen <anguyen@thunderhead.com>
2020-09-02 11:34:45 -0600

    Add Thunderhead SDK plugin code examples

--- ionic5/src/app/tab2/tab2.page.ts
+++ ionic5/src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var window:any;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+
+  ionViewDidEnter() {
+    var one = window.One;
+    one.sendInteraction("/tab2", null, 
+      function(response) {
+        console.log(response)
+      }, 
+      function(error) { 
+        console.log(error)
+      }
+    );
+  }
 
 }
--- ionic5/src/app/tab3/tab3.page.ts
+++ ionic5/src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var window:any;
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+
+  ionViewDidEnter() {
+    var one = window.One;
+    one.sendInteraction("/tab3", null, 
+      function(response) {
+        console.log(response)
+      }, 
+      function(error) { 
+        console.log(error)
+      }
+    );
+  }
 
 }
