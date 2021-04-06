121574a5d9aa2165b67bbcf5987998595b7b257c
Alex Nguyen <anguyen@thunderhead.com>
2021-04-05 20:21:12 -0500

    Use the native plugin when the platform is ready

--- examples/optimizing-programmatically-using-json-example/src/app/app.component.ts
+++ examples/optimizing-programmatically-using-json-example/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+declare var window:any;
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,27 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeOne();
+  }
+
+  private initializeOne() {
+    // The platform is ready and native functionality can be called.
+    // https://ionicframework.com/docs/angular/platform#ready-promise-string-
+    this.platform.ready().then(() => {
+      var one = window.One;
+
+      if (one) {
+        one.init({
+          siteKey: "ONE-XXXXXXXXXX-1022",
+          apiKey: "f713d44a-8af0-4e79-ba7e-xxxxxxxxxxxxxxxx",
+          sharedSecret: "bb8bacb2-ffc2-4c52-aaf4-xxxxxxxxxxxxxxxx",
+          userId: "api@yourCompanyName",
+          hostName: "https://xx.thunderhead.com",
+          touchpointURI: "ionic://optimization-example",
+          adminMode: false
+        });
+      }
+    });
+  }
 }
--- examples/optimizing-programmatically-using-json-example/src/app/tab1/tab1.page.ts
+++ examples/optimizing-programmatically-using-json-example/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, NgZone } from '@angular/core';
 import { ImageListItem } from '../models/image-list-item';
+import { Platform } from '@ionic/angular';
 
 const IMAGES: ImageListItem[] = [
   { src: 'assets/products/product-1A.png' },
@@ -11,7 +12,7 @@ const cardItemIdentifier = "card-item";
 const topBannerIdentifier = "top-banner";
 
 declare var window:any;
-let one = window.One;
+var one;
 
 @Component({
   selector: 'app-tab1',
@@ -23,49 +24,31 @@ export class Tab1Page {
   images: ImageListItem[] = IMAGES;
   myOptimizationData = {};
 
-  constructor(private zone: NgZone) {}
+  constructor(private zone: NgZone, private platform: Platform) {}
 
   ionViewDidEnter() {
-    one.sendInteraction("/tab1", null,
+    // The platform is ready and native functionality can be called.
+    // https://ionicframework.com/docs/angular/platform#ready-promise-string-
+    this.platform.ready().then(() => {
+      one = window.One;
+
+      one.sendInteraction("/tab1", null,
       (response) => {
         console.log("::tab1 Send Interaction response: ", response)
-
-        let newOptimizations = IMAGES
-
-        for (var optimization of response.optimizations) {
-          if (optimization.data) {
-            // base64 decode optimization data
-            let optimizationData = JSON.parse(atob(optimization.data))
-            let content = JSON.parse(optimizationData.actions[0].asset.content)
-            let responses = optimizationData.actions[0].asset.responses
-
-            if (optimization.path == topBannerIdentifier && content.image) {
-              console.log("::tab1 optimizing top banner: ", optimizationData.actions[0].name, content.image)
-              newOptimizations[0] = { src: content.image }
-            }
-            if (optimization.path == cardItemIdentifier && content.image) {
-              console.log("::tab1 optimizing card item: ", optimizationData.actions[0].name, content.image)
-              newOptimizations[1] = { src: content.image }
-            }
-
-            this.myOptimizationData[optimization.path] = { optimization: optimization, responses: responses }
-          }
-        }
-
-        this.zone.run(() => {
-          this.images = newOptimizations
-        })
+        this.optimizeContent(response)
       },
-      function(error) {
+      (error) => {
         console.log("::tab1 Send Interaction error: ", error)
+
         this.zone.run(() => {
           this.images = IMAGES
         });
       }
     );
+    });
   }
 
-  didSelectItem(index: number) {
+  private didSelectItem(index: number) {
     let optimizationPath: string = null
     if (index == 0) {
       optimizationPath = topBannerIdentifier
@@ -78,5 +61,33 @@ export class Tab1Page {
       console.log("Sending response code: ", response.code)
       one.sendResponseCode(response.code, "/tab1")
     }
+  }
+
+  private optimizeContent(response: any) {
+    let newOptimizations = IMAGES
+
+    for (var optimization of response.optimizations) {
+      if (optimization.data) {
+        // base64 decode optimization data
+        let optimizationData = JSON.parse(atob(optimization.data))
+        let content = JSON.parse(optimizationData.actions[0].asset.content)
+        let responses = optimizationData.actions[0].asset.responses
+
+        if (optimization.path == topBannerIdentifier && content.image) {
+          console.log("::tab1 optimizing top banner: ", optimizationData.actions[0].name, content.image)
+          newOptimizations[0] = { src: content.image }
+        }
+        if (optimization.path == cardItemIdentifier && content.image) {
+          console.log("::tab1 optimizing card item: ", optimizationData.actions[0].name, content.image)
+          newOptimizations[1] = { src: content.image }
+        }
+
+        this.myOptimizationData[optimization.path] = { optimization: optimization, responses: responses }
+      }
+    }
+
+    this.zone.run(() => {
+      this.images = newOptimizations
+    })
   }
 }
