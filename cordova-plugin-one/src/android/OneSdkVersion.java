16ae8a69e233ed9ae14c63d44d17192c66ef90b0
Alex Nguyen <anguyen@thunderhead.com>
2021-04-14 08:38:21 -0500

    Set version to 2.0.0

--- cordova-plugin-one/src/android/OneSdkVersion.java
+++ cordova-plugin-one/src/android/OneSdkVersion.java
@@ -1,7 +1,7 @@
 package com.thunderhead;
 
 public class OneSdkVersion {
-  private static final String ONE_CORDOVA_PLUGIN_VERSION = "1.5.0";
+  private static final String ONE_CORDOVA_PLUGIN_VERSION = "2.0.0";
 
   public static void updateModuleSdkVersion() {
     SdkExtraParams sdkExtraParams = new SdkExtraParams();
