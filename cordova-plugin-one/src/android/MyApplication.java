3dca5dc2e63d6e05ad58c976f9ed61fb0716c7b1
Alex Nguyen <anguyen@thunderhead.com>
2021-04-17 12:58:05 -0400

    Add script hook to add Application to Android project to disable codeless Interaction tracking, webview tracking, and outbound link tracking on app start.

---
+++ cordova-plugin-one/src/android/MyApplication.java
@@ -0,0 +1,22 @@
+package com.thunderhead;
+
+import android.app.Application;
+import com.thunderhead.android.api.codeless.OneCodelessInteractionTrackingConfiguration;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+      super.onCreate();
+
+      final OneCodelessInteractionTrackingConfiguration codelessInteractionTrackingConfiguration =
+        new OneCodelessInteractionTrackingConfiguration.Builder()
+          // disables Fragment/Activity Interaction Tracking
+          .disableCodelessInteractionTracking(true)
+          // disables WebView URL Interaction Tracking
+          .disableWebViewInteractionTracking(true)
+          // disables Outbound Link Tracking
+          .disableOutboundLinkTracking(true)
+          .build();
+      One.setCodelessInteractionTrackingConfiguration(codelessInteractionTrackingConfiguration);
+    }
+}
